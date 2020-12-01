02e62ce441088da5ff6995bf9d8e1b7535178ecf
Tim Su <tim@tandem.chat>
2020-11-30 16:18:48 -0800

    Change Swift active-win implementation to run forever and read from standard in

--- Sources/active-win/main.swift
+++ Sources/active-win/main.swift
@@ -1,3 +1,4 @@
+import Darwin.C
 import AppKit
 
 func toJson<T>(_ data: T) throws -> String {
@@ -5,55 +6,57 @@ func toJson<T>(_ data: T) throws -> String {
 	return String(data: json, encoding: .utf8)!
 }
 
-let frontmostAppPID = NSWorkspace.shared.frontmostApplication!.processIdentifier
-let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly, .excludeDesktopElements], kCGNullWindowID) as! [[String: Any]]
+func printFrontmostApp() {
+	let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly, .excludeDesktopElements], kCGNullWindowID) as! [[String: Any]]
 
-for window in windows {
-	let windowOwnerPID = window[kCGWindowOwnerPID as String] as! Int
+	// window list is ordered - first app is the top one
+	for window in windows {
+		// Skip transparent windows, like with Chrome
+		if (window[kCGWindowAlpha as String] as! Double) == 0 {
+			continue
+		}
 
-	if windowOwnerPID != frontmostAppPID {
-		continue
+		let bounds = CGRect(dictionaryRepresentation: window[kCGWindowBounds as String] as! CFDictionary)!
+
+		// Skip tiny windows, like the Chrome link hover statusbar
+		let minWinSize: CGFloat = 50
+		if bounds.width < minWinSize || bounds.height < minWinSize {
+			continue
+		}
+
+		let appPid = window[kCGWindowOwnerPID as String] as! pid_t
+
+		// This can't fail as we're only dealing with apps
+		let app = NSRunningApplication(processIdentifier: appPid)!
+		if app.bundleIdentifier == "com.apple.dock" {
+			continue
+		}
+
+		let dict: [String: Any] = [
+			"title": window[kCGWindowName as String] as? String ?? "",
+			"id": window[kCGWindowNumber as String] as! Int,
+			"bounds": [
+				"x": bounds.origin.x,
+				"y": bounds.origin.y,
+				"width": bounds.width,
+				"height": bounds.height
+			],
+			"owner": [
+				"name": window[kCGWindowOwnerName as String] as! String,
+				"processId": appPid,
+				"bundleId": app.bundleIdentifier!,
+				"path": app.bundleURL!.path
+			],
+			"memoryUsage": window[kCGWindowMemoryUsage as String] as! Int
+		]
+
+		print(try! toJson(dict))
+		fflush(stdout)
+		return
 	}
-
-	// Skip transparent windows, like with Chrome
-	if (window[kCGWindowAlpha as String] as! Double) == 0 {
-		continue
-	}
-
-	let bounds = CGRect(dictionaryRepresentation: window[kCGWindowBounds as String] as! CFDictionary)!
-
-	// Skip tiny windows, like the Chrome link hover statusbar
-	let minWinSize: CGFloat = 50
-	if bounds.width < minWinSize || bounds.height < minWinSize {
-		continue
-	}
-
-	let appPid = window[kCGWindowOwnerPID as String] as! pid_t
-
-	// This can't fail as we're only dealing with apps
-	let app = NSRunningApplication(processIdentifier: appPid)!
-
-	let dict: [String: Any] = [
-		"title": window[kCGWindowName as String] as? String ?? "",
-		"id": window[kCGWindowNumber as String] as! Int,
-		"bounds": [
-			"x": bounds.origin.x,
-			"y": bounds.origin.y,
-			"width": bounds.width,
-			"height": bounds.height
-		],
-		"owner": [
-			"name": window[kCGWindowOwnerName as String] as! String,
-			"processId": appPid,
-			"bundleId": app.bundleIdentifier!,
-			"path": app.bundleURL!.path
-		],
-		"memoryUsage": window[kCGWindowMemoryUsage as String] as! Int
-	]
-
-	print(try! toJson(dict))
-	exit(0)
 }
 
-print("null")
-exit(0)
+while(true) {
+	printFrontmostApp()
+	readLine()
+}
